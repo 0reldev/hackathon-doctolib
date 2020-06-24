7f4cb97fe50cc8c6ebef3d40da9c9f79c7c65691
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-06-24 09:33:47 +0200

    Project initilization

---
+++ src/main/java/com/jeboisducouscousetjenettoieunpaysan/hackathonDocotlib/HackathonDocotlibApplication.java
@@ -0,0 +1,13 @@
+package com.jeboisducouscousetjenettoieunpaysan.hackathonDocotlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonDocotlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonDocotlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeboisducouscousetjenettoieunpaysan/hackathonDocotlib/HackathonDocotlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeboisducouscousetjenettoieunpaysan.hackathonDocotlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonDocotlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
