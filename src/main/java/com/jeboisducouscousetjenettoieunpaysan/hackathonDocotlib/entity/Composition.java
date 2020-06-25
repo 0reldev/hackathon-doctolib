2a57d2bde85168baee0c6ba3be6a0c954dceab75
elisego <elise.godard@gmail.com>
2020-06-25 17:07:25 +0200

    correction nom entity

--- src/main/java/com/jeboisducouscousetjenettoieunpaysan/hackathonDocotlib/entity/Composition.java
+++ src/main/java/com/jeboisducouscousetjenettoieunpaysan/hackathonDocotlib/entity/Composition.java
@@ -3,6 +3,7 @@ package com.jeboisducouscousetjenettoieunpaysan.hackathonDocotlib.entity;
 import javax.persistence.*;
 
 @Entity
+@Table (name = "composition")
 public class Composition {
 
     @Id
@@ -24,5 +25,53 @@ public class Composition {
     private Drug drug;
 
     public Composition() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTimeOfTheDay() {
+        return timeOfTheDay;
+    }
+
+    public void setTimeOfTheDay(String timeOfTheDay) {
+        this.timeOfTheDay = timeOfTheDay;
+    }
+
+    public Pillbox getPillbox() {
+        return pillbox;
+    }
+
+    public void setPillbox(Pillbox pillbox) {
+        this.pillbox = pillbox;
+    }
+
+    public Drug getDrug() {
+        return drug;
+    }
+
+    public void setDrug(Drug drug) {
+        this.drug = drug;
     }
 }
