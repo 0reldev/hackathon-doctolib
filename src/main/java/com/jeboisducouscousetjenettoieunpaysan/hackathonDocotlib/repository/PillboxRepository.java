0f6c21d3ea0f8e246c863e4d112f4ed2ae082cd1
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-06-25 14:28:17 +0200

    Creation of repositories

---
+++ src/main/java/com/jeboisducouscousetjenettoieunpaysan/hackathonDocotlib/repository/PillboxRepository.java
@@ -0,0 +1,11 @@
+package com.jeboisducouscousetjenettoieunpaysan.hackathonDocotlib.repository;
+
+import com.jeboisducouscousetjenettoieunpaysan.hackathonDocotlib.entity.Pillbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PillboxRepository extends JpaRepository<Pillbox, Long> {
+
+
+}
